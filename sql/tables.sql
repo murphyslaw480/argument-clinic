78dccc18b09b154d977a7c73d3ce9d24ae34aca1
Ryan RC <murphyslaw480@gmail.com>
2014-03-16 17:15:02 -0400

    tag entity and text fields

--- sql/tables.sql
+++ sql/tables.sql
@@ -8,9 +8,9 @@ CREATE TABLE Users (
 
 CREATE TABLE Topic (  -- the heart of an argument
   id integer PRIMARY KEY,
-  stated_by varchar REFERENCES Users (name),
-  postdate date NOT NULL,
-  tags varchar  -- comma separated list of descriptive tags
+  text varchar NOT NULL,  -- a controversial statement to be argued about
+  creator varchar REFERENCES Users (name),
+  postdate date NOT NULL
   -- note: rage and logic scores are calculated by sum across arguments
   -- could use view
 );
@@ -26,18 +26,20 @@ CREATE TABLE Argument (   -- one discussion instance based on a statement
 );
 
 CREATE TABLE Opinion (   -- one expession of opinion in an argument
-  stated_by varchar REFERENCES Users (name), -- who made the comment
+  text varchar NOT NULL,                     -- text response to argument
+  posted_by varchar REFERENCES Users (name), -- who made the comment
   arg_id integer,                            -- argument instance where posted
   arg_topic integer,                         -- topic opinion is about
   postdate date NOT NULL,                    -- when it was posted
   rage integer DEFAULT 0,                    -- rage score (voted by others)
   logic integer DEFAULT 0,                   -- logic score (voted by others)
-  PRIMARY KEY (stated_by, arg_id, arg_topic),
+  PRIMARY KEY (posted_by, arg_id, arg_topic),
   -- an opinion must be associated with an argument
   FOREIGN KEY (arg_id, arg_topic) REFERENCES Argument (id, topic)
 );
 
 CREATE TABLE Comment (    -- comment on an opinion
+  text varchar NOT NULL,                     -- text response to opinion
   posted_by varchar REFERENCES Users (name), -- who made the comment
   target varchar,                            -- recipient of comment
   arg_id integer,                            -- argument based on statement
@@ -48,5 +50,11 @@ CREATE TABLE Comment (    -- comment on an opinion
   PRIMARY KEY (posted_by, target, arg_id, arg_topic),
   -- a comment is associated with a single opinion
   FOREIGN KEY (target, arg_id, arg_topic)
-    REFERENCES Opinion (stated_by, arg_id, arg_topic)
+    REFERENCES Opinion (posted_by, arg_id, arg_topic)
+);
+
+CREATE TABLE Tag (                        -- mapping of tags to topics
+  text varchar,                           -- tag descriptive text
+  topic integer REFERENCES Topic (id),    -- topic this was applied to
+  PRIMARY KEY (text, topic)
 );
