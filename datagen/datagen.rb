9217430712af9330323c8fba25595e21e132b0d9
Ryan RC <murphyslaw480@gmail.com>
2014-03-17 23:35:28 -0400

    start generation of topics

--- datagen/datagen.rb
+++ datagen/datagen.rb
@@ -3,19 +3,17 @@ require 'faker'
 #avoid warning from faker
 I18n.config.enforce_available_locales = true
 #Generate random data to populate database
+# select a random row from the given table
+# extract the values from columns specified as keys in column_map
+# rename those keys to the values in column_map
+def select_rand_row table, column_map
+  Hash[table.sample.map{|k,v| [column_map[k], v]}].delete_if{|k,v| k.nil?}
+end
 
-users_gen = {  
-  name: proc {Faker::Internet.user_name},
-  email: proc {Faker::Internet.email},
-  password: proc {Faker::Internet.password}
-}
-
-def generate_table generators, num_rows 
+def generate_table generators, num_rows
   column_names = generators.keys.map{|k| k.to_s}
   rows = []
   num_rows.times do
-    # call each generator to create a random value for that column
-    vals = generators.values.map{|g| g.call}
     # each row is a hash of column_name => value
     rows << Hash[generators.map{|k,v| [k, v.call]}]
   end
@@ -33,5 +31,32 @@ def generate_insert_statements table_name, table
   statement.chomp(",\n") + ';'
 end
 
+# create Users
+users_gen = {
+  "name" => proc {Faker::Internet.user_name},
+  "email" => proc {Faker::Internet.email},
+  "password" => proc {Faker::Internet.password}
+}
 users = generate_table users_gen, 10
+
+# create Topics
+topic_id = 0
+topic_gen = {
+  "id" => proc {topic_id += 1},
+  "text" => proc {Faker::Lorem.sentence},
+  "creator" => proc {users.sample["name"]}
+}
+topic = generate_table topic_gen, 10
+
+# create Arguments
+arg_id = [0] * (topic.length + 1)   # argument id counters
+current_topic = 0
+argument_gen = {
+  "topic" => proc {current_topic = topic.sample["id"]},
+  "id" => proc {arg_id[current_topic] += 1},
+}
+argument = generate_table argument_gen, 10
+
 puts generate_insert_statements "Users", users
+puts generate_insert_statements "Topic", topic
+puts generate_insert_statements "Argument", argument
