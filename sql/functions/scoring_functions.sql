e9429093ee54fa944ea7047a557841703bb4266b
Ryan RC <murphyslaw480@gmail.com>
2014-03-29 23:42:06 -0400

    scoring functions for arguments and topics

--- sql/functions/scoring_functions.sql
+++ sql/functions/scoring_functions.sql
@@ -32,7 +32,7 @@ AS $$
     AND vote.rage = true;
 $$ LANGUAGE SQL;
 
--- Tally up the logic score for a particular comment
+-- Tally up the logic score for a particular opinion
 CREATE OR REPLACE FUNCTION opinion_logic(
   opinion_poster varchar,
   arg_id integer,
@@ -47,7 +47,7 @@ AS $$
     AND vote.logic = true;
 $$ LANGUAGE SQL;
 
--- Tally up the logic score for a particular comment
+-- Tally up the rage score for a particular opinion
 CREATE OR REPLACE FUNCTION opinion_rage(
   opinion_poster varchar,
   arg_id integer,
@@ -59,5 +59,47 @@ AS $$
   WHERE vote.opinion_poster = $1
     AND vote.arg_id = $2
     AND vote.arg_topic = $3
+    AND vote.rage = true;
+$$ LANGUAGE SQL;
+
+-- Tally up the logic score for a particular argument
+CREATE OR REPLACE FUNCTION argument_rage(arg_id integer, arg_topic integer)
+RETURNS bigint
+AS $$
+  select count(*)
+  FROM OpinionVote vote
+  WHERE vote.arg_id = $1
+    AND vote.arg_topic = $2
+    AND vote.logic = true;
+$$ LANGUAGE SQL;
+
+-- Tally up the rage score for a particular argument
+CREATE OR REPLACE FUNCTION argument_rage(arg_id integer, arg_topic integer)
+RETURNS bigint
+AS $$
+  select count(*)
+  FROM OpinionVote vote
+  WHERE vote.arg_id = $1
+    AND vote.arg_topic = $2
+    AND vote.rage = true;
+$$ LANGUAGE SQL;
+
+-- Tally up the logic score for a particular topic
+CREATE OR REPLACE FUNCTION topic_logic(arg_topic integer)
+RETURNS bigint
+AS $$
+  select count(*)
+  FROM OpinionVote vote
+  WHERE vote.arg_topic = $1
+    AND vote.logic = true;
+$$ LANGUAGE SQL;
+
+-- Tally up the rage score for a particular topic
+CREATE OR REPLACE FUNCTION topic_rage(arg_topic integer)
+RETURNS bigint
+AS $$
+  select count(*)
+  FROM OpinionVote vote
+  WHERE vote.arg_topic = $1
     AND vote.rage = true;
 $$ LANGUAGE SQL;
